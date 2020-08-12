4a85a165c86340f774591781b44eca844ea65729
rahmaahassan <rh51796@gmail.com>
2020-08-12 19:23:28 +0200

    add google map and using it, current position

--- lib/home_page_screen.dart
+++ lib/home_page_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -10,38 +9,50 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final Map<String, Marker> _markers = {};
-
   GoogleMapController mapController;
-  //final LatLng _center = const LatLng(45.521563, -122.677433);
-  final Geolocator geolocator = Geolocator()..forceAndroidLocationManager;
 
+  final LatLng _center = const LatLng(45.521563, -122.677433);
 
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: _getLocation,
-        tooltip: 'Get Location',
-        child: Icon(Icons.flag),
-      ),
-      appBar: AppBar(
-        title: Text('Maps'),
-        centerTitle: true,
-      ),
-      body: GoogleMap(
-            mapType: MapType.hybrid,
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: CameraPosition(
-              target: LatLng(40.688841, -74.044015),
-              zoom: 11,
-            ),
-            markers: _markers.values.toSet(),
+    return MaterialApp(
+      home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: _getLocation,
+          tooltip: 'Get Location',
+          child: Icon(Icons.flag),
+
+        ),
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
+        ),
+        body: GoogleMap(
+          mapType: MapType.hybrid,
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+          markers: _markers.values.toSet(),
+        ),
       ),
     );
   }
 
   void _getLocation() async {
+
+    if (mapController != null) {
+      mapController.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(
+              target: _center,
+              zoom: 19.151926040649414))
+      );
+    }
     var currentLocation = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
 
@@ -54,26 +65,6 @@ class _HomePageState extends State<HomePage> {
       );
       _markers["Current Location"] = marker;
     });
-  }
-
-  /*getAddressFromLatLng() async {
-    try {
-      List<Placemark> p = await geolocator.placemarkFromCoordinates(
-        _currentPosition.latitude,
-        _currentPosition.longitude,
-      );
-      Placemark place = p[0];
-
-      setState(() {
-        _currentAddress = '${place.locality}, ${place.postalCode}, ${place.country}';
-      });
-    } catch (e) {
-      print (e);
-    }
-  }*/
-
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
   }
 
 }
